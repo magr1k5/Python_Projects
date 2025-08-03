48239b868ecee8cc43895d1f360b0369986b8727
magr1k5 <130539752+magr1k5@users.noreply.github.com>
2025-08-03 07:42:07 +0300

    Add files via upload

---
+++ pr18_zadanie2.py
@@ -0,0 +1,10 @@
+import random
+spisok = list(random.sample(range(1000),20))
+def Func(A):
+    B=[]
+    for i in A:
+        if i%2 ==1:
+            B.append(i)
+    print( ('Минимальное нечетное значение списка {} равно {}').format(A,min(B)))
+Func(spisok)   
+
